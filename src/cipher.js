1df38481ed311dc7f1711f066c52ef7d467876dc
lesliequispequispe <lesliequispe2605@gmail.com>
2019-02-26 11:38:30 -0500

    probando cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,43 @@
+/* creando el objeto global cipher,  window.cipher */
+
+
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+
+  encode: (offset, string) => {
+  let codificar = "";
+  let offsetNumero = parseInt(offset,10);
+  
+  for (let i = 0; i < string.length; i++){
+  //console.log(string[i]);
+  //string.charCodeAt(0);
+  //console.log(string.charCodeAt(0));
+  //console.log(string.charCodeAt(i));
+  let valorAssci = string.charCodeAt(i);
+  
+   codificar += String.fromCharCode((valorAssci- 65 + offsetNumero) % 26 + 65);
+  
+  
   }
-};
+  return (codificar);
+  
+  },
+  
+  decode: (offset, string) => {
+      let decodificar = "";
+      let offsetNumero2 = parseInt(offset,10);
+      
+      for (let i = 0; i < string.length; i++){
+      //console.log(string[i]);
+      //string.charCodeAt(0);
+      //console.log(string.charCodeAt(0));
+      //console.log(string.charCodeAt(i));
+      let valorAssci2 = string.charCodeAt(i);
+      
+       decodificar += String.fromCharCode((valorAssci2- offsetNumero2+ 65) % 26 + 65);
+      
+      
+      }
+      return (decodificar);
+      
+  }
+  }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-/* Acá va tu código */
+
+const encode = document.getElementById("encode");
+
+const descifrar = document.getElementById("descifrar");
+
+const text = document.getElementById("mensaje");
+
+const displace = document.getElementById("displace");
+
+const result = document.getElementById("result");
+
+const limpiar = document.getElementById("limpiar");
+
+
+encode.addEventListener("click",() => {
+
+document.getElementById("result").innerHTML=cipher.encode(displace.value, text.value);
+
+})
+
+descifrar.addEventListener("click", () =>{
+//console.log(decode);
+document.getElementById("result").innerHTML=cipher.decode(displace.value, text.value);
+
+})
+
+limpiar.addEventListener("click", () => {
+
+
+})
